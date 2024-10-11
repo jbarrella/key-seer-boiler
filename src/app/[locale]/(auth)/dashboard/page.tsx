320005692fe8e37a530528c991a5e2a1e9c5b05f
Jason Barrella <jmbtis@gmail.com>
2024-10-11 12:07:36 +0200

    refactor(boilerplate): customizing the boilerplate

--- src/app/[locale]/(auth)/dashboard/page.tsx
+++ src/app/[locale]/(auth)/dashboard/page.tsx
@@ -1,22 +1,3 @@
-import { getTranslations } from 'next-intl/server';
-
-import { Hello } from '@/components/Hello';
-
-export async function generateMetadata(props: { params: { locale: string } }) {
-  const t = await getTranslations({
-    locale: props.params.locale,
-    namespace: 'Dashboard',
-  });
-
-  return {
-    title: t('meta_title'),
-  };
+export default function Dashboard() {
+  return <div>Dashboard</div>;
 }
-
-const Dashboard = () => (
-  <div className="[&_p]:my-6">
-    <Hello />
-  </div>
-);
-
-export default Dashboard;
